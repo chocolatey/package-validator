75be4e2808a1d908de8da313c343cfea7ba924b9
Gary Ewan Park <gep13@gep13.co.uk>
2015-11-25 12:01:29 +0000

    (rules) Added ReleaseNotesNotEmptyGuideline

---
+++ src/chocolatey.package.validator/infrastructure.app/rules/ReleaseNotesNotEmptyGuideline.cs
@@ -0,0 +1,30 @@
+﻿// Copyright © 2015 - Present RealDimensions Software, LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+//
+// You may obtain a copy of the License at
+//
+// 	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+namespace chocolatey.package.validator.infrastructure.app.rules
+{
+    using NuGet;
+    using infrastructure.rules;
+
+    public class ReleaseNotesNotEmptyGuideline : BasePackageRule
+    {
+        public override string ValidationFailureMessage { get { return "Release Notes (releaseNotes) are a short description of changes in each version of a package. Please include releasenotes in the nuspec. **NOTE:** To prevent the need to continually update this field, providing a URL to an external list of Release Notes is perfectly acceptable."; } }
+
+        protected override PackageValidationOutput is_valid(IPackage package)
+        {
+            return !string.IsNullOrWhiteSpace(package.ReleaseNotes.to_string());
+        }
+    }
+}
