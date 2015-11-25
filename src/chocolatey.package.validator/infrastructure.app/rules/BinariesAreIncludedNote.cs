894be257d5960771ee7580d9c48dc6862d7640d4
Rob Reynolds <ferventcoder@gmail.com>
2015-11-25 04:12:31 -0600

    (rules) Add more rules
    - BinariesAreIncludedNote
    - BugTrackerUrlMissingSuggestion
    - PackageSourceUrlMissingGuideline
    - ProjectSourceUrlDoesNotMatchProjectUrlGuideline
    - ProjectSourceUrlMissingSuggestion

---
+++ src/chocolatey.package.validator/infrastructure.app/rules/BinariesAreIncludedNote.cs
@@ -0,0 +1,31 @@
+﻿// Copyright © 2015 - Present RealDimensions Software, LLC
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// 
+// You may obtain a copy of the License at
+// 
+// 	http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+namespace chocolatey.package.validator.infrastructure.app.rules
+{
+    using System.Linq;
+    using NuGet;
+    using infrastructure.rules;
+
+    public class BinariesAreIncludedNote : BasePackageRule
+    {
+        public override string ValidationFailureMessage { get { return "Binary files (.exe) have been included. The reviewer will ensure you have distribution rights."; } }
+
+        protected override PackageValidationOutput is_valid(IPackage package)
+        {
+            return !package.GetFiles().or_empty_list_if_null().Any(f => f.Path.to_lower().EndsWith(".exe"));
+        }
+    }
+}
---
+++ src/chocolatey.package.validator/infrastructure.app/rules/BugTrackerUrlMissingSuggestion.cs
@@ -0,0 +1,30 @@
+// Copyright © 2015 - Present RealDimensions Software, LLC
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// 
+// You may obtain a copy of the License at
+// 
+// 	http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+namespace chocolatey.package.validator.infrastructure.app.rules
+{
+    using NuGet;
+    using infrastructure.rules;
+
+    public class BugTrackerUrlMissingSuggestion : BasePackageRule
+    {
+        public override string ValidationFailureMessage { get { return "The nuspec has been enhanced to allow you to put in bugTrackerUrl, pointing to the location where issues and tickets can be accessed. Please consider adding it to the nuspec, if available."; } }
+
+        protected override PackageValidationOutput is_valid(IPackage package)
+        {
+            return !string.IsNullOrWhiteSpace(package.BugTrackerUrl.to_string());
+        }
+    }
+}
