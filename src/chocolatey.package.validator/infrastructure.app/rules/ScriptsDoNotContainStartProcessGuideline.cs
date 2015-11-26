e4a001f07b889c2dc511ece9a135ce17a65f4110
Gary Ewan Park <gep13@gep13.co.uk>
2015-11-26 20:56:52 +0000

    (GH-10) Added check for call to Start-Process
    - To ensure that the Chocolatey Package Scripts do not have any calls to
    Start-Process
    - Fixes GH-10

---
+++ src/chocolatey.package.validator/infrastructure.app/rules/ScriptsDoNotContainStartProcessGuideline.cs
@@ -0,0 +1,47 @@
+﻿// Copyright © 2015 - Present RealDimensions Software, LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+//
+// You may obtain a copy of the License at
+//
+// 	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+namespace chocolatey.package.validator.infrastructure.app.rules
+{
+    using System.IO;
+    using NuGet;
+    using infrastructure.rules;
+
+    public class ScriptsDoNotContainStartProcessGuideline : BasePackageRule
+    {
+        public override string ValidationFailureMessage { get { return @"Your script contains a call to Start-Process. This is not recommended, and can most likely be replaced by using the Chocolatey Helper Function called Start-ChocolateyProcessAsAdmin.  This could also flag a comment or message with the following words:
+  * Start-Process
+  ";} }
+
+        protected override PackageValidationOutput is_valid(IPackage package)
+        {
+            var valid = true;
+
+            var files = package.GetFiles().or_empty_list_if_null();
+            foreach (var packageFile in files)
+            {
+                string extension = Path.GetExtension(packageFile.Path).to_lower();
+                if (extension != ".ps1" && extension != ".psm1") continue;
+
+                var contents = packageFile.GetStream().ReadToEnd().to_lower();
+
+                if (contents.Contains("Start-Process"))
+                    valid = false;
+            }
+
+            return valid;
+        }
+    }
+}
