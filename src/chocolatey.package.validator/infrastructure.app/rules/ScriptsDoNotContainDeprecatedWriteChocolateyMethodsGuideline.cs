3894d9f71827a8087820f69d3b02eec84cf6ecd9
Rob Reynolds <ferventcoder@gmail.com>
2015-12-01 01:11:22 -0600

    (GH-28) Write-ChocolateySuccess/Failure are not used

---
+++ src/chocolatey.package.validator/infrastructure.app/rules/ScriptsDoNotContainDeprecatedWriteChocolateyMethodsGuideline.cs
@@ -0,0 +1,53 @@
+﻿// Copyright © 2015 - Present RealDimensions Software, LLC
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// 
+// You may obtain a copy of the License at
+// 
+// 	http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+namespace chocolatey.package.validator.infrastructure.app.rules
+{
+    using System.IO;
+    using NuGet;
+    using infrastructure.rules;
+
+    public class ScriptsDoNotContainDeprecatedWriteChocolateyMethodsGuideline : BasePackageRule
+    {
+        public override string ValidationFailureMessage
+        {
+            get
+            {
+                return
+                    @"Write-ChocolateySuccess/Write-ChocolateyFailure have been deprecated, but have been found in your automation scripts. Remove the usage of those two functions. If you have an error, throw the error. This can also give a false positive for commented code with the words:
+  * Write-ChocolateySuccess
+  * Write-ChocolateyFailure";
+            }
+        }
+
+        protected override PackageValidationOutput is_valid(IPackage package)
+        {
+            var valid = true;
+
+            var files = package.GetFiles().or_empty_list_if_null();
+            foreach (var packageFile in files)
+            {
+                string extension = Path.GetExtension(packageFile.Path).to_lower();
+                if (extension != ".ps1" && extension != ".psm1") continue;
+
+                var contents = packageFile.GetStream().ReadToEnd().to_lower();
+
+                if (contents.Contains("write-chocolateysuccess") || contents.Contains("write-chocolateyfailure")) valid = false;
+            }
+
+            return valid;
+        }
+    }
+}
