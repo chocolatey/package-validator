6891e67472abce79d317d7ed13c2e7fea9744ddd
Rob Reynolds <ferventcoder@gmail.com>
2015-12-05 07:51:47 -0600

    (GH-21) Submit final results to website
    When receiving a message, submit those results to the validate endpoint
    on the website.

--- src/chocolatey.package.validator/infrastructure.app/tasks/UpdateWebsiteInformationTask.cs
+++ src/chocolatey.package.validator/infrastructure.app/tasks/UpdateWebsiteInformationTask.cs
@@ -16,15 +16,13 @@
 namespace chocolatey.package.validator.infrastructure.app.tasks
 {
     using System;
-    using System.Linq;
     using System.Net;
     using System.Text;
+    using NuGet;
     using configuration;
     using infrastructure.messaging;
-    using infrastructure.rules;
     using infrastructure.tasks;
     using messaging;
-    using NuGet;
     using registration;
     using services;
     using HttpUtility = System.Web.HttpUtility;
@@ -43,9 +41,8 @@ namespace chocolatey.package.validator.infrastructure.app.tasks
 
         public void initialize()
         {
-            //todo Point at a new message
-           // _subscription = EventManager.subscribe<PackageValidationResultMessage>(update_website, null, null);
-            this.Log().Info(() => "{0} is now ready and waiting for PackageValidationResultMessage".format_with(GetType().Name));
+            _subscription = EventManager.subscribe<FinalPackageValidationResultMessage>(update_website, null, null);
+            this.Log().Info(() => "{0} is now ready and waiting for {1}".format_with(GetType().Name, typeof(FinalPackageValidationResultMessage).Name));
         }
 
         public void shutdown()
@@ -55,24 +52,21 @@ namespace chocolatey.package.validator.infrastructure.app.tasks
 
         public event EventHandler<WebRequestEventArgs> SendingRequest = delegate { };
 
-        //todo: update service endpoint.
-        private const string SERVICE_ENDPOINT = "/api/v2/nowhere";
+        private const string SERVICE_ENDPOINT = "/api/v2/validate";
 
-        private void update_website(PackageValidationResultMessage message)
+        private void update_website(FinalPackageValidationResultMessage message)
         {
-            var failedRequired = message.ValidationResults.Any(r => r.Validated == false && r.ValidationLevel == ValidationLevelType.Requirement);
-            
-            this.Log().Info(() => "Updating website for {0} v{1} with results (package {2} requirements).".format_with(message.PackageId, message.PackageVersion, failedRequired ? "failed" : "passed"));
+            this.Log().Info(() => "Updating website for {0} v{1} with results (package {2} requirements).".format_with(message.PackageId, message.PackageVersion, message.Success ? "failed" : "passed"));
 
             try
             {
                 var url = string.Join("/", SERVICE_ENDPOINT, message.PackageId, message.PackageVersion);
                 HttpClient client = _nugetService.get_client(_configurationSettings.PackagesUrl, url, "POST", "application/x-www-form-urlencoded");
 
-                StringBuilder postData = new StringBuilder();
+                var postData = new StringBuilder();
                 postData.Append("apikey=" + HttpUtility.UrlEncode(_configurationSettings.PackagesApiKey));
-                //postData.Append("&success=" + HttpUtility.UrlEncode(message.Success.to_string().to_lower()));
-                //postData.Append("&resultDetailsUrl=" + HttpUtility.UrlEncode(message.ResultDetailsUrl));
+                postData.Append("&success=" + HttpUtility.UrlEncode(message.Success.to_string().to_lower()));
+                postData.Append("&validationComments=" + HttpUtility.UrlEncode(message.ValidationMessage));
                 var form = postData.ToString();
                 var data = Encoding.ASCII.GetBytes(form);
 
@@ -98,7 +92,5 @@ namespace chocolatey.package.validator.infrastructure.app.tasks
                 Bootstrap.handle_exception(ex);
             }
         }
-
-        // EventManager.publish(new WebsiteUpdateMessage());
     }
 }
