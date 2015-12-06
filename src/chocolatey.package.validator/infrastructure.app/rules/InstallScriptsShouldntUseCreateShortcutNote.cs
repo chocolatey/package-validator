d1275f8168a7a1355e1712fcdb2f887d3911ca44
Gary Ewan Park <gep13@gep13.co.uk>
2015-12-06 20:09:50 +0000

    (GH-35) Don't use .CreateShortcut Note
    - Closes GH-35

---
+++ src/chocolatey.package.validator/infrastructure.app/rules/InstallScriptsShouldntUseCreateShortcutNote.cs
@@ -0,0 +1,45 @@
+﻿// Copyright © 2015 - Present RealDimensions Software, LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+//
+// You may obtain a copy of the License at
+//
+// 	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+namespace chocolatey.package.validator.infrastructure.app.rules
+{
+    using System.IO;
+    using infrastructure.rules;
+    using NuGet;
+
+    public class InstallScriptsShouldntUseCreateShortcutNote : BasePackageRule
+    {
+        public override string ValidationFailureMessage { get { return "Installation Scripts are using .CreateShortcut.  Suggest to maintainer that Chocolatey Helper for creating Shortcuts is used instead.  Please see [here](https://github.com/chocolatey/package-validator/wiki/UsageOfCreateShortcut) for further information and guidance."; } }
+
+        public override PackageValidationOutput is_valid(IPackage package)
+        {
+            var valid = true;
+
+            var files = package.GetFiles().or_empty_list_if_null();
+
+            foreach (var packageFile in files)
+            {
+                string extension = Path.GetExtension(packageFile.Path).to_lower();
+                if (extension != ".ps1" && extension != ".psm1") continue;
+
+                var contents = packageFile.GetStream().ReadToEnd().to_lower();
+
+                if (contents.Contains(".createshortcut")) valid = false;
+            }
+
+            return valid;
+        }
+    }
+}
