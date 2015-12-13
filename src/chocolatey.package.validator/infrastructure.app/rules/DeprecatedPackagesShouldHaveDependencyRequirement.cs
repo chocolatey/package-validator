3c7e496e1c4c1c6a2537a72211bf1c5219b05559
Gary Ewan Park <gep13@gep13.co.uk>
2015-12-13 21:13:56 +0000

    (GH-60) Deprecated Package Dependency Requirement

---
+++ src/chocolatey.package.validator/infrastructure.app/rules/DeprecatedPackagesShouldHaveDependencyRequirement.cs
@@ -0,0 +1,33 @@
+﻿// Copyright © 2015 - Present RealDimensions Software, LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+//
+// You may obtain a copy of the License at
+//
+// 	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+namespace chocolatey.package.validator.infrastructure.app.rules
+{
+    using System.Linq;
+    using NuGet;
+    using infrastructure.rules;
+
+    public class DeprecatedPackagesShouldHaveDependencyRequirement : BasePackageRule
+    {
+        public override string ValidationFailureMessage { get { return "Deprecated Packages must contain a reference to the package which it is being replaced by.  Please see [wiki article](https://github.com/chocolatey/package-validator/wiki/DeprecatedPackagesMustHaveADependency) for further information and guidance."; } }
+
+        public override PackageValidationOutput is_valid(IPackage package)
+        {
+            if (!package.Title.to_lower().Contains("deprecated")) return true;
+
+            return !package.DependencySets.Any();
+        }
+    }
+}
