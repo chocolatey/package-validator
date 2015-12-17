ab2d6905e53c6f5e03e3a4d141352643f0f7af12
Gary Ewan Park <gep13@gep13.co.uk>
2015-12-17 21:44:15 +0000

    (GH-71) Nuspec should be UTF-8 Guideline

---
+++ src/chocolatey.package.validator/StreamExtensions.cs
@@ -0,0 +1,43 @@
+﻿// Copyright © 2015 - Present RealDimensions Software, LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+//
+// You may obtain a copy of the License at
+//
+// 	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+namespace chocolatey.package.validator
+{
+    using System.IO;
+    using System.Text;
+
+    public static class StreamExtensions
+    {
+        // Adapted from blog post
+        // https://weblog.west-wind.com/posts/2007/Nov/28/Detecting-Text-Encoding-for-StreamReader
+        public static Encoding get_encoding(this Stream file)
+        {
+            // *** Use Default of Encoding.Default (Ansi CodePage)
+            var enc = Encoding.Default;
+
+            // *** Detect byte order mark if any - otherwise assume default
+            var buffer = new byte[5];
+            file.Read(buffer, 0, 5);
+            file.Close();
+
+            if (buffer[0] == 0xef && buffer[1] == 0xbb && buffer[2] == 0xbf) enc = Encoding.UTF8;
+            else if (buffer[0] == 0xfe && buffer[1] == 0xff) enc = Encoding.Unicode;
+            else if (buffer[0] == 0 && buffer[1] == 0 && buffer[2] == 0xfe && buffer[3] == 0xff) enc = Encoding.UTF32;
+            else if (buffer[0] == 0x2b && buffer[1] == 0x2f && buffer[2] == 0x76) enc = Encoding.UTF7;
+
+            return enc;
+        }
+    }
+}
---
+++ src/chocolatey.package.validator/infrastructure.app/rules/NuspecFileShouldBeUtf8Guideline.cs
@@ -0,0 +1,49 @@
+﻿// Copyright © 2015 - Present RealDimensions Software, LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+//
+// You may obtain a copy of the License at
+//
+// 	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+namespace chocolatey.package.validator.infrastructure.app.rules
+{
+    using System.IO;
+    using System.Text;
+    using chocolatey.package.validator.infrastructure.rules;
+    using NuGet;
+
+    public class NuspecFileShouldBeUtf8Guideline : BasePackageRule
+    {
+        public override string ValidationFailureMessage { get { return
+@"Nuspec file should be UTF-8 encoded. [More...](https://github.com/chocolatey/package-validator/wiki/NuspecFileShouldBeUtf8)";
+        }
+        }
+
+        public override PackageValidationOutput is_valid(IPackage package)
+        {
+            var valid = true;
+
+            var files = package.GetFiles().or_empty_list_if_null();
+
+            foreach (var packageFile in files)
+            {
+                string extension = Path.GetExtension(packageFile.Path).to_lower();
+                if (extension != ".nuspec") continue;
+
+                var encoding = packageFile.GetStream().get_encoding();
+
+                if (!(encoding == Encoding.UTF8 || encoding == Encoding.Default)) valid = false;
+            }
+
+            return valid;
+        }
+    }
+}
