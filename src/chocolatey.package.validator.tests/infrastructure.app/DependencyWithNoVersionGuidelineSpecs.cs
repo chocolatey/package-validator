b3ae02ac8aa42d9dd3b3a38f3acc1cc23756b624
Rob Reynolds <ferventcoder@gmail.com>
2015-12-21 22:24:30 -0600

    (GH-84) Min version should not flag no version
    The specs surrounding minimum version check were checking against a
    version spec that included both minimum and maximum versions. Update
    specs to add a version that only specifies minimum version and fix the
    check.

--- src/chocolatey.package.validator.tests/infrastructure.app/DependencyWithNoVersionGuidelineSpecs.cs
+++ src/chocolatey.package.validator.tests/infrastructure.app/DependencyWithNoVersionGuidelineSpecs.cs
@@ -1,12 +1,12 @@
 ﻿// Copyright © 2015 - Present RealDimensions Software, LLC
-//
+// 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-//
+// 
 // You may obtain a copy of the License at
-//
+// 
 // 	http://www.apache.org/licenses/LICENSE-2.0
-//
+// 
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,23 +16,70 @@
 namespace chocolatey.package.validator.tests.infrastructure.app
 {
     using System.Collections.Generic;
-    using System.Runtime.Versioning;
-    using chocolatey.package.validator.infrastructure.app.rules;
-    using chocolatey.package.validator.infrastructure.rules;
     using Moq;
     using NuGet;
     using Should;
+    using validator.infrastructure.app.rules;
+    using validator.infrastructure.rules;
 
     public abstract class DependencyWithNoVersionGuidelineSpecsBase : TinySpec
     {
         protected DependencyWithNoVersionGuideline guideline;
         protected Mock<IPackage> package = new Mock<IPackage>();
-        protected PackageDependency packageDependencyWithVersion = new PackageDependency("fiddler", new VersionSpec(new SemanticVersion(1, 0, 0, 0)));
+        protected PackageDependency packageDependencyWithMinVersion;
+        protected PackageDependency packageDependencyWithMaxVersion;
         protected PackageDependency packageDependencyWithNoVersion = new PackageDependency("chocolatey");
 
         public override void Context()
         {
+            var minVersionSpec = new VersionSpec
+            {
+                MinVersion = new SemanticVersion("1.0.0"),
+                IsMinInclusive = true
+            };
+            packageDependencyWithMinVersion = new PackageDependency("fiddler", minVersionSpec);
+
+            var minAndMaxVersionSpec = new VersionSpec
+            {
+                MinVersion = new SemanticVersion("1.0.0"),
+                MaxVersion = new SemanticVersion("2.0.0"),
+                IsMinInclusive = true
+            };
+            packageDependencyWithMaxVersion = new PackageDependency("dude", minAndMaxVersionSpec);
+
             guideline = new DependencyWithNoVersionGuideline();
+        }
+
+        public class when_inspecting_a_package_that_has_no_dependencies : DependencyWithNoVersionGuidelineSpecsBase
+        {
+            private PackageValidationOutput result;
+
+            public override void Context()
+            {
+                base.Context();
+
+                package.Setup(p => p.DependencySets).Returns( new List<PackageDependencySet>
+                {
+                    new PackageDependencySet(null, new List<PackageDependency>())
+                });
+            }
+
+            public override void Because()
+            {
+                result = guideline.is_valid(package.Object);
+            }
+
+            [Fact]
+            public void should_be_valid()
+            {
+                result.Validated.ShouldBeTrue();
+            }
+
+            [Fact]
+            public void should_not_override_the_base_message()
+            {
+                result.ValidationFailureMessageOverride.ShouldBeNull();
+            }
         }
 
         public class when_inspecting_single_dependency_that_has_no_version : DependencyWithNoVersionGuidelineSpecsBase
@@ -43,14 +90,14 @@ namespace chocolatey.package.validator.tests.infrastructure.app
             {
                 base.Context();
                 var packageDependencies = new List<PackageDependency>
-            {
-                packageDependencyWithNoVersion
-            };
-                var packageDependencySet = new PackageDependencySet(new FrameworkName(".NETFramework, Version = 4.0"), packageDependencies);
+                {
+                    packageDependencyWithNoVersion
+                };
+                var packageDependencySet = new PackageDependencySet(null, packageDependencies);
                 package.Setup(p => p.DependencySets).Returns(
                     new List<PackageDependencySet>
                     {
-                    packageDependencySet
+                        packageDependencySet
                     });
             }
 
@@ -80,15 +127,15 @@ namespace chocolatey.package.validator.tests.infrastructure.app
             {
                 base.Context();
                 var packageDependencies = new List<PackageDependency>
-            {
-                packageDependencyWithNoVersion,
-                this.packageDependencyWithVersion
-            };
-                var packageDependencySet = new PackageDependencySet(new FrameworkName(".NETFramework, Version = 4.0"), packageDependencies);
+                {
+                    packageDependencyWithNoVersion,
+                    packageDependencyWithMinVersion
+                };
+                var packageDependencySet = new PackageDependencySet(null, packageDependencies);
                 package.Setup(p => p.DependencySets).Returns(
                     new List<PackageDependencySet>
                     {
-                    packageDependencySet
+                        packageDependencySet
                     });
             }
 
@@ -110,7 +157,7 @@ namespace chocolatey.package.validator.tests.infrastructure.app
             }
         }
 
-        public class when_inspecting_single_dependency_that_has_version : DependencyWithNoVersionGuidelineSpecsBase
+        public class when_inspecting_single_dependency_that_has_min_version : DependencyWithNoVersionGuidelineSpecsBase
         {
             private PackageValidationOutput result;
 
@@ -118,14 +165,51 @@ namespace chocolatey.package.validator.tests.infrastructure.app
             {
                 base.Context();
                 var packageDependencies = new List<PackageDependency>
-            {
-                packageDependencyWithVersion
-            };
-                var packageDependencySet = new PackageDependencySet(new FrameworkName(".NETFramework, Version = 4.0"), packageDependencies);
+                {
+                    packageDependencyWithMinVersion
+                };
+                var packageDependencySet = new PackageDependencySet(null, packageDependencies);
                 package.Setup(p => p.DependencySets).Returns(
                     new List<PackageDependencySet>
                     {
-                    packageDependencySet
+                        packageDependencySet
+                    });
+            }
+
+            public override void Because()
+            {
+                result = guideline.is_valid(package.Object);
+            }
+
+            [Fact]
+            public void should_be_valid()
+            {
+                result.Validated.ShouldBeTrue();
+            }
+
+            [Fact]
+            public void should_not_override_the_base_message()
+            {
+                result.ValidationFailureMessageOverride.ShouldBeNull();
+            }
+        }   
+        
+        public class when_inspecting_single_dependency_that_has_max_version : DependencyWithNoVersionGuidelineSpecsBase
+        {
+            private PackageValidationOutput result;
+
+            public override void Context()
+            {
+                base.Context();
+                var packageDependencies = new List<PackageDependency>
+                {
+                    packageDependencyWithMaxVersion
+                };
+                var packageDependencySet = new PackageDependencySet(null, packageDependencies);
+                package.Setup(p => p.DependencySets).Returns(
+                    new List<PackageDependencySet>
+                    {
+                        packageDependencySet
                     });
             }
 
--- src/chocolatey.package.validator/infrastructure.app/rules/DependencyWithNoVersionGuideline.cs
+++ src/chocolatey.package.validator/infrastructure.app/rules/DependencyWithNoVersionGuideline.cs
@@ -1,12 +1,12 @@
 ﻿// Copyright © 2015 - Present RealDimensions Software, LLC
-//
+// 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-//
+// 
 // You may obtain a copy of the License at
-//
+// 
 // 	http://www.apache.org/licenses/LICENSE-2.0
-//
+// 
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,17 +16,23 @@
 namespace chocolatey.package.validator.infrastructure.app.rules
 {
     using System.Linq;
-    using infrastructure.rules;
     using NuGet;
+    using infrastructure.rules;
 
     public class DependencyWithNoVersionGuideline : BasePackageRule
     {
-        public override string ValidationFailureMessage { get { return 
-@"Package contains dependencies with no specified version. You should at least specify a minimum version of a dependency. [More...](https://github.com/chocolatey/package-validator/wiki/DependencyWithNoVersion)"; } }
+        public override string ValidationFailureMessage
+        {
+            get
+            {
+                return
+                    @"Package contains dependencies with no specified version. You should at least specify a minimum version of a dependency. [More...](https://github.com/chocolatey/package-validator/wiki/DependencyWithNoVersion)";
+            }
+        }
 
         public override PackageValidationOutput is_valid(IPackage package)
         {
-            return !package.DependencySets.Any(dependencySet => dependencySet.Dependencies.Any(dependency => dependency.VersionSpec == null || dependency.VersionSpec.MaxVersion == null || dependency.VersionSpec.MinVersion == null));
+            return !package.DependencySets.Any(dependencySet => dependencySet.Dependencies.Any(dependency => dependency.VersionSpec == null || dependency.VersionSpec.MinVersion == null));
         }
     }
 }
