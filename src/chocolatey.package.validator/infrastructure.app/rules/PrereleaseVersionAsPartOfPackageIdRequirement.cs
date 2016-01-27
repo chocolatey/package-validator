480ba693de15bc945c7d678ec06007d6c3de4b27
Gary Ewan Park <gep13@gep13.co.uk>
2016-01-27 20:53:09 +0000

    (GH-92) No Version Info in ID Requirement

---
+++ src/chocolatey.package.validator/infrastructure.app/rules/PrereleaseVersionAsPartOfPackageIdRequirement.cs
@@ -0,0 +1,41 @@
+﻿// Copyright © 2015 - Present RealDimensions Software, LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+//
+// You may obtain a copy of the License at
+//
+// 	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+namespace chocolatey.package.validator.infrastructure.app.rules
+{
+    using infrastructure.rules;
+    using NuGet;
+
+    public class PrereleaseVersionAsPartOfPackageIdRequirement : BasePackageRule
+    {
+        public override string ValidationFailureMessage
+        {
+            get
+            {
+                return
+@"The package id includes a prerelease version name which should be included only in the version of the package. [More...](https://github.com/chocolatey/package-validator/wiki/PrereleaseVersionAsPartOfPackageId)";
+            }
+        }
+
+        public override PackageValidationOutput is_valid(IPackage package)
+        {
+            var packageId = package.Id.to_lower();
+
+            return !(packageId.Contains("alpha")
+                || packageId.Contains("beta")
+                || packageId.Contains("prerelease"));
+        }
+    }
+}
