319426c22d2e7539560f6482aee872baded67083
Gary Ewan Park <gep13@gep13.co.uk>
2016-02-18 19:24:06 +0000

    (GH-90) Added Note for using underscore

---
+++ src/chocolatey.package.validator.tests/infrastructure.app/PackageIdUsesUnderscoresNoteSpecs.cs
@@ -0,0 +1,92 @@
+﻿// Copyright © 2015 - Present RealDimensions Software, LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+//
+// You may obtain a copy of the License at
+//
+// 	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+namespace chocolatey.package.validator.tests.infrastructure.app
+{
+    using chocolatey.package.validator.infrastructure.app.rules;
+    using chocolatey.package.validator.infrastructure.rules;
+    using Moq;
+    using NuGet;
+    using Should;
+
+    public abstract class PackageIdUsesUnderscoresNoteSpecsBase : TinySpec
+    {
+        protected PackageIdUsesUnderscoresNote note;
+        protected Mock<IPackage> package = new Mock<IPackage>();
+
+        public override void Context()
+        {
+            note = new PackageIdUsesUnderscoresNote();
+        }
+
+        public class when_inspecting_package_with_underscore_in_id : PackageIdUsesUnderscoresNoteSpecsBase
+        {
+            private PackageValidationOutput result;
+
+            public override void Context()
+            {
+                base.Context();
+
+                package.Setup(p => p.Id).Returns("package_A");
+            }
+
+            public override void Because()
+            {
+                result = note.is_valid(package.Object);
+            }
+
+            [Fact]
+            public void should_not_be_valid()
+            {
+                result.Validated.ShouldBeFalse();
+            }
+
+            [Fact]
+            public void should_not_override_the_base_message()
+            {
+                result.ValidationFailureMessageOverride.ShouldBeNull();
+            }
+        }
+
+        public class when_inspecting_package_without_underscore_in_id : PackageIdUsesUnderscoresNoteSpecsBase
+        {
+            private PackageValidationOutput result;
+
+            public override void Context()
+            {
+                base.Context();
+
+                package.Setup(p => p.Id).Returns("packageA");
+            }
+
+            public override void Because()
+            {
+                result = note.is_valid(package.Object);
+            }
+
+            [Fact]
+            public void should_be_valid()
+            {
+                result.Validated.ShouldBeTrue();
+            }
+
+            [Fact]
+            public void should_not_override_the_base_message()
+            {
+                result.ValidationFailureMessageOverride.ShouldBeNull();
+            }
+        }
+    }
+}
---
+++ src/chocolatey.package.validator/infrastructure.app/rules/PackageIdUsesUnderscoresNote.cs
@@ -0,0 +1,37 @@
+﻿// Copyright © 2015 - Present RealDimensions Software, LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+//
+// You may obtain a copy of the License at
+//
+// 	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+namespace chocolatey.package.validator.infrastructure.app.rules
+{
+    using infrastructure.rules;
+    using NuGet;
+
+    public class PackageIdUsesUnderscoresNote : BasePackageRule
+    {
+        public override string ValidationFailureMessage
+        {
+            get
+            {
+                return
+@"The package id includes underscorse (_). Usually the package id is separated by '-' instead of underscores.  The reviewer will ensure this is not a new package. [More...](https://github.com/chocolatey/package-validator/wiki/PackageIdUsesUnderscores)";
+            }
+        }
+
+        public override PackageValidationOutput is_valid(IPackage package)
+        {
+            return !package.Id.Contains("_");
+        }
+    }
+}
