ff28a5a00bce2b94f55b0d553454580c956b8cae
Gary Ewan Park <gep13@gep13.co.uk>
2019-12-16 16:26:14 +0000

    (GH-194) Add rule and tests for markdown headings
    To validate that any headings used within the Description field of the
    nuspec file contains valid markdown syntax.  Before the update to the
    chocolatey.org website, it would correctly render a heading that used:
    
    However, this is accurately against the markdown spec, and instead
    should be written as follows:
    
    This new rule, will provide a Guideline in the package valdiator output
    to suggest that the correct syntax be used.

---
+++ src/chocolatey.package.validator/infrastructure.app/rules/DescriptionHeadingMarkdownRequirement.cs
@@ -0,0 +1,47 @@
+﻿// Copyright © 2015 - Present RealDimensions Software, LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+//
+// You may obtain a copy of the License at
+//
+// 	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+namespace chocolatey.package.validator.infrastructure.app.rules
+{
+    using System.Text.RegularExpressions;
+    using infrastructure.rules;
+    using NuGet;
+
+    public class DescriptionHeadingMarkdownRequirement : BasePackageRule
+    {
+        private const string HeadingRegexPattern = @"^(#+)([^\s#].*)$";
+
+        public override string ValidationFailureMessage
+        {
+            get
+            {
+                return
+@"Package Description should not contain invalid Markdown Headings. [More...](https://github.com/chocolatey/package-validator/wiki/DescriptionValidMarkdownHeading)";
+            }
+        }
+
+        public override PackageValidationOutput is_valid(IPackage package)
+        {
+            if (package.Description == null)
+            {
+                return true;
+            }
+
+            var regex = new Regex(HeadingRegexPattern, RegexOptions.Multiline);
+
+            return !regex.IsMatch(package.Description);
+        }
+    }
+}
