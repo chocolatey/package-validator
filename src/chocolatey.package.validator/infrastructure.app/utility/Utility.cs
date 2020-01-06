510c410872ce7b60b0feb12f32ccce953b36f457
Gary Ewan Park <gep13@gep13.co.uk>
2020-01-06 09:21:19 +0000

    (maint) Remove whitespace

--- src/chocolatey.package.validator/infrastructure.app/utility/Utility.cs
+++ src/chocolatey.package.validator/infrastructure.app/utility/Utility.cs
@@ -1,4 +1,4 @@
-﻿// Copyright © 2015 - Present RealDimensions Software, LLC
+// Copyright © 2015 - Present RealDimensions Software, LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -101,7 +101,6 @@ namespace chocolatey.package.validator.infrastructure.app.utility
                     StringExtensions.to_lower(f.Path).EndsWith(".dmg") ||
                     StringExtensions.to_lower(f.Path).EndsWith(".so")  ||
                     StringExtensions.to_lower(f.Path).EndsWith(".jar")
-
             );
         }
 
