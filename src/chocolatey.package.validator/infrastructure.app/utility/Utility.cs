acae0d1094afe2b9c0e5fe824c61625786a994a9
Gary Ewan Park <gep13@gep13.co.uk>
2020-07-02 14:45:09 +0100

    (GH-235) Change how exceptions are handled
    When using HttpClient, there is no longer a WebException thrown, so
    validation has to be handled slightly differently.

--- src/chocolatey.package.validator/infrastructure.app/utility/Utility.cs
+++ src/chocolatey.package.validator/infrastructure.app/utility/Utility.cs
@@ -137,48 +137,49 @@ namespace chocolatey.package.validator.infrastructure.app.utility
 
                 client.DefaultRequestHeaders.Add("Connection", "keep-alive");
 
-                message.Headers.Add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9");
-                message.Headers.Add("Accept-Language", "en-GB,en-US;q=0.8,en;q=0.6,de-DE;q=0.4,de;q=0.2");
                 message.Headers.Add("Upgrade-Insecure-Requests", "1");
                 message.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36");
-                message.Headers.Add("Accept-Encoding", "gzip, deflate, br");
+                message.Headers.Add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9");
                 message.Headers.Add("Sec-Fetch-Mode", "navigate");
                 message.Headers.Add("Sec-Fetch-Dest", "document");
                 message.Headers.Add("Sec-Fetch-Site", "cross-site");
                 message.Headers.Add("Sec-Fetch-User", "?1");
+                message.Headers.Add("Accept-Encoding", "gzip, deflate, br");
+                message.Headers.Add("Accept-Language", "en-GB,en-US;q=0.8,en;q=0.6,de-DE;q=0.4,de;q=0.2");
 
                 var response = client.SendAsync(message).GetAwaiter().GetResult();
-                return response.StatusCode == HttpStatusCode.OK;
-            }
-            catch (WebException ex)
-            {
-                if (ex.Status == System.Net.WebExceptionStatus.ProtocolError && ex.Message == "The remote server returned an error: (403) Forbidden." && ex.Response.Headers["Server"] == "cloudflare")
+
+                if (response.StatusCode == HttpStatusCode.Forbidden && response.Headers.Server.ToString() == "cloudflare")
                 {
-                    "package-validator".Log().Warn("Error validating Url {0} - {1}", url.ToString(), ex.Message);
+                    "package-validator".Log().Warn("Received a Forbidden response validating Url {0}", url.ToString());
                     "package-validator".Log().Warn("Since this is likely due to the fact that the server is using Cloudflare, is sometimes popping up a Captcha which needs to be solved, obviously not possible by package-validator.");
                     "package-validator".Log().Warn("This check was put in place as a result of this issue: https://github.com/chocolatey/package-validator/issues/229");
                     return true;
                 }
 
-                if (ex.Status == WebExceptionStatus.SecureChannelFailure || (ex.Status == WebExceptionStatus.UnknownError && ex.Message == "The SSL connection could not be established, see inner exception. Unable to read data from the transport connection: An existing connection was forcibly closed by the remote host."))
-                {
-                    "package-validator".Log().Warn("Error validating Url {0} - {1}", url.ToString(), ex.Message);
-                    "package-validator".Log().Warn("Since this is likely due to missing Ciphers on the machine hosting package-validator, this URL will be marked as valid for the time being.");
-                    return true;
-                }
-
-                if (ex.Status == WebExceptionStatus.ProtocolError && ex.Message == "The remote server returned an error: (503) Server Unavailable.")
-                {
-                    "package-validator".Log().Warn("Error validating Url {0} - {1}", url.ToString(), ex.Message);
-                    "package-validator".Log().Warn("This could be due to Cloudflare DDOS protection acting in front of the site, or another valid reason, as such, this URL will be marked as valid for the time being.");
-                    return true;
-                }
-
-                "package-validator".Log().Warn("Error validating Url {0} - {1}", url.ToString(), ex.Message);
-                return false;
+                return response.StatusCode == HttpStatusCode.OK;
             }
             catch (Exception ex)
             {
+                var exceptions = new List<Exception>();
+                var currentException = ex;
+
+                while (currentException.InnerException != null)
+                {
+                    exceptions.Add(currentException);
+                    currentException = currentException.InnerException;
+                }
+
+                foreach(var exception in exceptions)
+                {
+                    if (exception.Message == "Unable to read data from the transport connection: An existing connection was forcibly closed by the remote host." || exception.Message == "An existing connection was forcibly closed by the remote host")
+                    {
+                        "package-validator".Log().Warn("Error validating Url {0} - {1}", url.ToString(), ex.Message);
+                        "package-validator".Log().Warn("Since this is likely due to missing Ciphers on the machine hosting package-validator, this URL will be marked as valid for the time being.");
+                        return true;
+                    }
+                }
+
                 "package-validator".Log().Warn("Error validating Url {0} - {1}", url.ToString(), ex.Message);
                 return false;
             }
